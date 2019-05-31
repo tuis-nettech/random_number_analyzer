f2dfd49cde6c03b5cee418bd9c1fbfcd0ae43f3c
salschoka <mimutsuyo3624@gmail.com>
2019-06-01 01:08:56 +0900

    hoge

---
+++ numgame.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+int main()
+{
+    printf("--==num game==--\n\n");
+// generate a random number
+
+    //Wanna get MilliSec to make better randomnumber
+    int num = 30;
+    printf("Guess the number from 0 to 99.\n");
+    printf("The answer is %d.\n",num);
+    // start the game
+    int ans=0;
+// start main rutine
+    for(int num;num=!ans;num+0)
+    {
+        printf("Enter your answer.\n");
+        scanf("%d",&ans);
+        printf("You enterd %d.\n",ans);
+        
+        if (ans == num)
+        {
+            printf("Collect!!\n");
+            break;
+        }
+        else if (ans > num)
+            {
+                printf("Too huge.\n");
+            }
+            else
+            {
+                printf("Too small.\n");
+            };
+    printf("The answer is %d.\n",num);
+    };
+}
